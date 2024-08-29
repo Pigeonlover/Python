08f45f30861ef35592d5da8b6a6f1678dd7a4981
Pigeonlover <bravefoot@outlook.com>
2024-08-29 16:23:55 +0100

    Final edits

--- Day1.py
+++ Day1.py
@@ -1,3 +1,12 @@
-name = input("What is your name?")
-print("Hello "+name+", your name has ",len(name)," letters!")
+# Band Name Generator
 
+# name = input("What is your name?")
+# print("Hello "+name+", your name has ",len(name)," letters!")
+
+print("Welcome to the Band Name Generator!")
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What pet do you own, or would like to own?\n")
+
+print("Your suggested band name is", city.title(), pet.title(),"!")
